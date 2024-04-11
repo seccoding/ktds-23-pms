936b8c582baeea57af7408e00e1f45c0e89e1f89
Seyoung Jung <syjung022722@gmail.com>
2024-04-11 16:21:41 +0900

    요구사항,산출물 관리 패키지 기본 클래스 설정, 해당 패키지 매퍼파일 생성

---
+++ src/main/java/com/ktdsuniversity/edu/pms/requirement/vo/RequirementVO.java
@@ -0,0 +1,159 @@
+package com.ktdsuniversity.edu.pms.requirement.vo;
+
+public class RequirementVO {
+	
+	private String rqmId;
+	private String rqmCntnt;
+	private String strtDt;
+	private String endDt;
+	private String rqmFile;
+	private String dvlrp;
+	private String cfrmr;
+	private String tstr;
+	private String testRslt;
+	private String scdSts;
+	private String rqmSts;
+	private String crtDt;
+	private String crtrId;
+	private String mdfDt;
+	private String mdfrId;
+	private String prjId;
+	private String delYn;
+
+	public String getRqmId() {
+		return rqmId;
+	}
+
+	public void setRqmId(String rqmId) {
+		this.rqmId = rqmId;
+	}
+
+	public String getRqmCntnt() {
+		return rqmCntnt;
+	}
+
+	public void setRqmCntnt(String rqmCntnt) {
+		this.rqmCntnt = rqmCntnt;
+	}
+
+	public String getStrtDt() {
+		return strtDt;
+	}
+
+	public void setStrtDt(String strtDt) {
+		this.strtDt = strtDt;
+	}
+
+	public String getEndDt() {
+		return endDt;
+	}
+
+	public void setEndDt(String endDt) {
+		this.endDt = endDt;
+	}
+
+	public String getRqmFile() {
+		return rqmFile;
+	}
+
+	public void setRqmFile(String rqmFile) {
+		this.rqmFile = rqmFile;
+	}
+
+	public String getDvlrp() {
+		return dvlrp;
+	}
+
+	public void setDvlrp(String dvlrp) {
+		this.dvlrp = dvlrp;
+	}
+
+	public String getCfrmr() {
+		return cfrmr;
+	}
+
+	public void setCfrmr(String cfrmr) {
+		this.cfrmr = cfrmr;
+	}
+
+	public String getTstr() {
+		return tstr;
+	}
+
+	public void setTstr(String tstr) {
+		this.tstr = tstr;
+	}
+
+	public String getTestRslt() {
+		return testRslt;
+	}
+
+	public void setTestRslt(String testRslt) {
+		this.testRslt = testRslt;
+	}
+
+	public String getScdSts() {
+		return scdSts;
+	}
+
+	public void setScdSts(String scdSts) {
+		this.scdSts = scdSts;
+	}
+
+	public String getRqmSts() {
+		return rqmSts;
+	}
+
+	public void setRqmSts(String rqmSts) {
+		this.rqmSts = rqmSts;
+	}
+
+	public String getCrtDt() {
+		return crtDt;
+	}
+
+	public void setCrtDt(String crtDt) {
+		this.crtDt = crtDt;
+	}
+
+	public String getCrtrId() {
+		return crtrId;
+	}
+
+	public void setCrtrId(String crtrId) {
+		this.crtrId = crtrId;
+	}
+
+	public String getMdfDt() {
+		return mdfDt;
+	}
+
+	public void setMdfDt(String mdfDt) {
+		this.mdfDt = mdfDt;
+	}
+
+	public String getMdfrId() {
+		return mdfrId;
+	}
+
+	public void setMdfrId(String mdfrId) {
+		this.mdfrId = mdfrId;
+	}
+
+	public String getPrjId() {
+		return prjId;
+	}
+
+	public void setPrjId(String prjId) {
+		this.prjId = prjId;
+	}
+
+	public String getDelYn() {
+		return delYn;
+	}
+
+	public void setDelYn(String delYn) {
+		this.delYn = delYn;
+	}
+
+}
