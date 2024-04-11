d0d9b8e3696ab3f1fe7ccb75de3360323eae986c
211-20\User <kwan0507@nate.com>
2024-04-11 19:40:06 +0900

    [2024-04-11] [김형관] A,Bteam 에러 코드 수정 (ChangeHistoryDaoImpl에 sqlSessionTemplate 메소드 추가)

--- src/main/java/com/ktdsuniversity/edu/pms/changehistory/dao/ChangeHistoryDaoImpl.java
+++ src/main/java/com/ktdsuniversity/edu/pms/changehistory/dao/ChangeHistoryDaoImpl.java
@@ -1,9 +1,16 @@
 package com.ktdsuniversity.edu.pms.changehistory.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ChangeHistoryDaoImpl extends SqlSessionDaoSupport implements ChangeHistoryDao{
 
+	@Autowired
+	@Override
+	public void setSqlSessionTemplate(SqlSessionTemplate sqlSessionTemplate) {
+		super.setSqlSessionTemplate(sqlSessionTemplate);
+	}
 }
