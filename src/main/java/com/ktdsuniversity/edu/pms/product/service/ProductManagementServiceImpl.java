ce52bc9edcd7c9947e029a4ce0af55e7232e0c90
Sohyun Kim <ksh96611@naver.com>
2024-04-11 16:59:34 +0900

    product(비품, 비품관리), borrow(대여이력) 패키지 추가

---
+++ src/main/java/com/ktdsuniversity/edu/pms/product/service/ProductManagementServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ktdsuniversity.edu.pms.product.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductManagementServiceImpl implements ProductManagementService{
+
+}
