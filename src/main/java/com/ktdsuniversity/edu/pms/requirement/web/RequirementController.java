bf2d866ffd2ff82f46d42c45372eaf89ead1cc60
Seyoung Jung <syjung022722@gmail.com>
2024-04-12 09:25:19 +0900

    [2024-04-12][정세영] 요구사항 todo작성 및 yaml파일에 파일저장경로 변경

--- src/main/java/com/ktdsuniversity/edu/pms/requirement/web/RequirementController.java
+++ src/main/java/com/ktdsuniversity/edu/pms/requirement/web/RequirementController.java
@@ -1,7 +1,10 @@
 package com.ktdsuniversity.edu.pms.requirement.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -14,36 +17,59 @@ public class RequirementController {
 
 	@Autowired
 	private RequirementService requirementService;
-	
+
 //	@GetMapping("")
-//	public AjaxResponse getAllRequirement(/*@SessionAttribute ,*/ String prjId) {
-//		return null;
-//		
-//	}
-//
+	public String getAllRequirement(/* @SessionAttribute , */ String prjId, Model model) {
+		// TODO
+//		1. fillter interceptor 
+//		요청자 정보가 비로그인 유져면 out 
+//		요청자 정보가 시스템관리자인지? & 팀원정보가 어떤것인지
+
+//		2. select 
+//		요구사항리스트를 가져온다 
+//		1번의 정보에 따른 스트림으로 걸러내기
+//		List<RequirementVO> requirementList = requirementService.getAllRequirement(prjId)
+//		.stream()
+//		.filter(()->
+//			if()
+//		).tolist();
+
+//		3. model에 주입 
+//		model.addAllAttributes(null)
+
+//		4. go jsp fileName
+		return null;
+
+	}
+
 //	@GetMapping("")
-//	public AjaxResponse getOneRequirement(/*@SessionAttribute ,*/ String rqmId) {
-//		return null;
-//
-//	}
+	public AjaxResponse getOneRequirement(/* @SessionAttribute , */ String rqmId) {
+
+		return null;
+
+	}
+
 //	@PostMapping("")
-//	public AjaxResponse modifyRequirement(/*@SessionAttribute ,*/RequirementVO requirementVO) {
-//		return null;
-//
-//	}
+	public AjaxResponse modifyRequirement(/* @SessionAttribute , */RequirementVO requirementVO) {
+		return null;
+
+	}
+
 //	@PostMapping("")
-//	public AjaxResponse deleteRequirement(/*@SessionAttribute ,*/ RequirementVO requirementVO) {
-//		return null;
-//
-//	}
-//	public AjaxResponse delayRequirement(/*@SessionAttribute ,*/ String rqmId) {
-//		return null;
-//
-//	}
+	public AjaxResponse deleteRequirement(/* @SessionAttribute , */ RequirementVO requirementVO) {
+		return null;
+
+	}
+
+	public AjaxResponse delayRequirement(/* @SessionAttribute , */ String rqmId) {
+		return null;
+
+	}
+
 //	@PostMapping("")
-//	public AjaxResponse approveDelay(/*@SessionAttribute ,*/ String rqmId) {
-//		return null;
-//
-//	}
+	public AjaxResponse approveDelay(/* @SessionAttribute , */ String rqmId) {
+		return null;
+
+	}
 
 }
