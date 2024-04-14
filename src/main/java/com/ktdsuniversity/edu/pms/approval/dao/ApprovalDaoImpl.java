c9aedd6b04bf86d2e02331ac0001a93ac190ca22
Songhwa Park <hello.sh.day@gmail.com>
2024-04-14 16:28:15 +0900

    [2024-04-14][박송화] 결재 기능 추가 - 전체 목록 조회

--- src/main/java/com/ktdsuniversity/edu/pms/approval/dao/ApprovalDaoImpl.java
+++ src/main/java/com/ktdsuniversity/edu/pms/approval/dao/ApprovalDaoImpl.java
@@ -1,9 +1,13 @@
 package com.ktdsuniversity.edu.pms.approval.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.ktdsuniversity.edu.pms.approval.vo.ApprovalVO;
 
 @Repository
 public class ApprovalDaoImpl extends SqlSessionDaoSupport implements ApprovalDao {
@@ -12,6 +16,16 @@ public class ApprovalDaoImpl extends SqlSessionDaoSupport implements ApprovalDao
 	@Override
 	public void setSqlSessionTemplate(SqlSessionTemplate sqlSessionTemplate) {
 		super.setSqlSessionTemplate(sqlSessionTemplate);
+	}
+
+	@Override
+	public int getAllCount() {
+		return getSqlSession().selectOne(ApprovalDao.NAME_SPACE + ".getAllCount");
+	}
+
+	@Override
+	public List<ApprovalVO> getAllApproval() {
+		return getSqlSession().selectList(ApprovalDao.NAME_SPACE + ".getAllApproval");
 	}
 	
 }
