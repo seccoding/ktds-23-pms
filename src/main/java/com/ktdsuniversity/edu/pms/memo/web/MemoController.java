0395fde8043831473966b0f772a2d015624b0046
BumJin Jo <jobumjin5500@gmail.com>
2024-04-15 19:15:40 +0900

    [2024-04-15][조범진] 받은쪽지목록 만들기 기본

--- src/main/java/com/ktdsuniversity/edu/pms/memo/web/MemoController.java
+++ src/main/java/com/ktdsuniversity/edu/pms/memo/web/MemoController.java
@@ -2,12 +2,25 @@ package com.ktdsuniversity.edu.pms.memo.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.ktdsuniversity.edu.pms.memo.service.MemoService;
+import com.ktdsuniversity.edu.pms.memo.vo.MemoListVO;
 
 @Controller
 public class MemoController {
 	
 	@Autowired
 	private MemoService memoService; 
+	
+	@GetMapping("/memo/sentlist")
+	public String viewSentMemoListPage(Model model) {
+		
+		MemoListVO memoListVO =this.memoService.getSentMemoAllsearch();
+		
+		model.addAttribute("memoList", memoListVO );
+		
+		return "/memo/sentlist";
+	}
 }
