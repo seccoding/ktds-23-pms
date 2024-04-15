a91e204fd5ad054410d8678bcd2e57c453476879
Seyoung Jung <syjung022722@gmail.com>
2024-04-15 10:01:00 +0900

    [2024-04-15] [정세영] 요구사항 업데이트

--- src/main/java/com/ktdsuniversity/edu/pms/requirement/web/RequirementController.java
+++ src/main/java/com/ktdsuniversity/edu/pms/requirement/web/RequirementController.java
@@ -32,25 +32,17 @@ public class RequirementController {
 	 */
 	@GetMapping("/project/{prjId}/requirement")
 	public String viewAllRequirement(
-			/* @SessionAttribute ,*/ 
-			@PathVariable("prjId")  String prjId, 
-			Model model) {
+			/* @SessionAttribute , */
+			@PathVariable("prjId") String prjId, Model model) {
 		// TODO
 //		1. fillter interceptor 
 //		요청자 정보가 비로그인 유져면 out 
 //		요청자 정보가 시스템관리자인지? & 팀원정보가 어떤것인지
-
-//		2. select 
-//		프로젝트에 요구사항이 생성되잇는지 확인하고
-//		요구사항이 체크되있으면
-//		요구사항리스트를 가져온다 
-//		1번의 정보에 따른 스트림으로 걸러내기
-		List<RequirementVO> requirementList= requirementService.getAllRequirement()
-				.stream().filter( (requirement)-> requirement.getPrjId().equals(prjId))
-				.collect(Collectors.toList());
-//		3. model에 주입 
-		model.addAttribute("resultList",requirementList);
-
+//		2. select and filter
+		List<RequirementVO> requirementList = requirementService.getAllRequirement().stream()
+				.filter((requirement) -> requirement.getPrjId().equals(prjId)).collect(Collectors.toList());
+//		3. model injection 
+		model.addAttribute("resultList", requirementList);
 //		4. go jsp fileName
 		return "requirement/requirementlist";
 
@@ -58,64 +50,52 @@ public class RequirementController {
 
 	@GetMapping("/project/{prjId}/requirement/{rqmId}")
 	public String viewOneRequirement(
-			/* @SessionAttribute , */ 
-			@PathVariable("prjId")  String prjId,
-			@PathVariable("rqmId") String rqmId, 
-			Model model) {
+			/* @SessionAttribute , */
+			@PathVariable("prjId") String prjId, @PathVariable("rqmId") String rqmId, Model model) {
+		// TODO
 //		1. fillter interceptor 
 //		요청자 정보가 비로그인 유져면 out 
 //		요청자 정보가 시스템관리자인지? & 팀원정보가 어떤것인지
-
 //		2. select 
-//		프로젝트에 요구사항이 생성되잇는지 확인하고
-//		요구사항이 체크되있으면
-//		요구사항을 가져온다 
-//		본인이 볼수있는 요구사항인지 확인
 		RequirementVO requirement = this.requirementService.getOneRequirement(rqmId);
-
-//		3. model에 주입 
+//		3. model injection 
 		model.addAttribute("requirement", requirement);
-
 //		4. go jsp fileName
 		return "requirement/requirementview";
 
 	}
 
-
 	@GetMapping("/project/requirement/write")
 	public String viewwritePage(
-			/* @SessionAttribute , */) {
+	/* @SessionAttribute , */) {
 //		1. fillter interceptor 
 //		요청자 정보가 비로그인 유져면 out 
 //		요청자 정보가 시스템관리자인지? & 팀원정보가 어떤것인지
-		
-		
+
+//		2. go jsp
 		return "requirement/requirementwrite";
-		
+
 	}
+
 	@ResponseBody
 	@PostMapping("/project/{prjId}/requirement/write")
 	public String createRequirement(/* @SessionAttribute , */
-			@PathVariable("prjId")  String prjId,
-			RequirementVO requirementVO,
-			@RequestParam MultipartFile file,
+			@PathVariable("prjId") String prjId, RequirementVO requirementVO, @RequestParam MultipartFile file,
 			Model model) {
 //		1. fillter interceptor 
 //		요청자 정보가 비로그인 유져면 out 
 //		요청자 정보가 시스템관리자인지? & 팀원정보가 어떤것인지
-		
-		boolean isSuccess = this.requirementService.insertOneRequirement(requirementVO,file);
-		
+
+		boolean isSuccess = this.requirementService.insertOneRequirement(requirementVO, file);
+
 //		4. isSuccess ==true -> redirect jsp
-		return "redirect:/project/"+prjId;
-		
+		return "redirect:/project/" + prjId;
+
 	}
-	
+
 	@GetMapping("/project/{prjId}/requirement/{rqmId}/write")
 	public String viewModifyPage(/* @SessionAttribute , */
-			@PathVariable("prjId") String prjId,
-			@PathVariable("rqmId") String rqmId,
-			Model model) {
+			@PathVariable("prjId") String prjId, @PathVariable("rqmId") String rqmId, Model model) {
 //		1. fillter interceptor 
 //		요청자 정보가 비로그인 유져면 out 
 //		요청자 정보가 시스템관리자인지? & 팀원정보가 어떤것인지
@@ -134,47 +114,44 @@ public class RequirementController {
 		return "requirement/requirementwrite";
 
 	}
-	
-	
+
 //	@ResponseBody
 //	@PostMapping("")
 	public String modifyRequirement(
 			/* @SessionAttribute , */
-			@PathVariable String rqmId,
-			RequirementVO requirementVO,
-			@RequestParam MultipartFile file) {
+			@PathVariable String rqmId, RequirementVO requirementVO, @RequestParam MultipartFile file) {
 //		1. fillter interceptor 
 //		요청자 정보가 비로그인 유져면 out 
 //		요청자 정보가 시스템관리자인지? & 팀원정보가 어떤것인지
-		
+
 //		2.필수정보 입력 확인
-		
+
 //		3.내용 업데이트 
 		boolean isSuccess = this.requirementService.updateRequirement(requirementVO, file);
-		
+
 //		4. isSuccess ==true -> redirect jsp
 		return null;
 
 	}
-	
+
 //	@ResponseBody
 //	@PostMapping("")
 	public String deleteRequirement(
-			/* @SessionAttribute , */ 
+			/* @SessionAttribute , */
 			@PathVariable String rqmId) {
 //		1. fillter interceptor 
 //		요청자 정보가 비로그인 유져면 out 
 //		요청자 정보가 시스템관리자인지? & 팀원정보가 어떤것인지
-		
+
 //		2. 본인이나 관리자라면 
 //		삭제코드 실행 
 		boolean isSuccess = this.requirementService.deleteOneRequirement(rqmId);
-		
+
 //		4. isSuccess ==true -> redirect jsp
 		return null;
 
 	}
-	
+
 //	@ResponseBody
 //	@PostMapping("")
 
@@ -182,31 +159,28 @@ public class RequirementController {
 //		1. fillter interceptor 
 //		요청자 정보가 비로그인 유져면 out 
 //		요청자 정보가 시스템관리자인지? & 팀원정보가 어떤것인지
-		
-		
+
 //		2. 연기요청 처리
-		RequirementVO thisRequirement =this.requirementService.getOneRequirement(rqmId);
-		//setter 로 정보 변경후 업데이트
+		RequirementVO thisRequirement = this.requirementService.getOneRequirement(rqmId);
+		// setter 로 정보 변경후 업데이트
 		boolean isSuccess = this.requirementService.updateRequirement(thisRequirement, null);
-		
+
 //		3. ajax result return 
 		AjaxResponse ajax = new AjaxResponse();
-		ajax.append("result", isSuccess);		
+		ajax.append("result", isSuccess);
 		return ajax;
 
 	}
-	
 
 //	@ResponseBody
 //	@PostMapping("")
 	public AjaxResponse accessDelay(
-			/* @SessionAttribute , */ 
-			@PathVariable String rqmId,
-			@RequestParam boolean dalayApprove) {
+			/* @SessionAttribute , */
+			@PathVariable String rqmId, @RequestParam boolean dalayApprove) {
 //		1. fillter interceptor 
 //		요청자 정보가 비로그인 유져면 out 
 //		요청자 정보가 시스템관리자인지? & 팀원정보가 어떤것인지
-		
+
 //		2. 승인인지 거절인지 확인 
 //		그 값에 따라 연기 요청 처리
 //		boolean isSuccess = this.requirementService.updateDelayRequirement(rqmId, true or false);
@@ -214,7 +188,7 @@ public class RequirementController {
 //		3. ajax result return 
 //		AjaxResponse ajax = new AjaxResponse();
 //		ajax.append("result", isSuccess);
-		
+
 //		return ajax;
 		return null;
 	}
--- src/main/webapp/WEB-INF/views/requirement/requirementwrite.jsp
+++ src/main/webapp/WEB-INF/views/requirement/requirementwrite.jsp
@@ -17,22 +17,44 @@ pageEncoding="UTF-8"%> <%@ taglib prefix="c" uri="jakarta.tags.core" %>
 
   <body>
     <div class="grid">
-      <div>프로젝트명 ${requirememt.projectVO.prjName}</div>
-      <input
-        type="text"
-        name="prgId"
-        value="${requirememt.projectVO.prjName}"
-      />
-      <div>요구사항 아이디</div>
-      <input type="text" name="rqmId" value="${requirement.rqmId}" />
+      <c:if test="${not empty requirememt.projectVO.prjName}">
+        <div>프로젝트명</div>
+        <div>${requirememt.projectVO.prjName}</div>
+      </c:if>
+
       <div>요구사항 제목</div>
-      <div>${requirement.rqmTtl}</div>
+      <input type="text" name="rqmId" value="${requirement.rqmTtl}" />
+
       <div>요구사항 내용</div>
-      <div>${requirement.rqmCntnt}</div>
-      <div>요구사항 상태1</div>
-      <div>${requirement.scdStsVO.cmcdName}</div>
-      <div>요구사항 상태1</div>
-      <div>${requirement.rqmStsVO.cmcdName}</div>
+      <input type="text" name="rqmId" value="${requirement.rqmCntnt}" />
+      <!--체크박스 선택창-->
+      <div>일정상태</div>
+      <select name="scdStsVOCmcdName" id="scdStsVO-cmcdName">
+        <option value="501">대기중</option>
+        <option value="502">진행중</option>
+        <!-- <option value="503" >연기필요</option> -->
+      </select>
+
+      <!--체크박스 선택창-->
+      <div>진행상태</div>
+      <select name="rqmStsVOCmcdName" id="rqmStsVO-cmcdName">
+        <option value="601">분석중</option>
+        <option value="602">설계중</option>
+        <option value="603">개발중</option>
+        <option value="604">단위테스트진행중</option>
+        <option value="605">개발완료</option>
+      </select>
+
+      <!--날짜선택창-->
+      <label for="start-date">시작일</label>
+      <input type="date" name="start-date" value="${requirement.strtDt}" />
+      <!--날짜선택창-->
+      <label for="end-date">종료예정일</label>
+      <input type="date" name="end-date" value="${requirement.endDt}" />
+
+      <!--담당자 확인자 테스터 테스트결과는 아직 어떤 기준으로 해야하는지 알 수 없음-->
+
+      <button id="submitBtn" type="button" a="">제출</button>
     </div>
   </body>
 </html>
