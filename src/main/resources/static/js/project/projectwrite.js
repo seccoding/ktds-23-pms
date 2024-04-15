094e3d3c8b4a9b490190ee986ef2f7051ec29acb
CHOI, HYUNGJUN <2018s4030@gs.anyang.ac.kr>
2024-04-15 09:59:01 +0900

    [2024-04-15] [최형준] 공통기능 추가 - Front Date Check

--- src/main/resources/static/js/project/projectwrite.js
+++ src/main/resources/static/js/project/projectwrite.js
@@ -30,51 +30,6 @@ $().ready(function () {
         }
     });
 
-
-    // 날짜 체크 1, 프로젝트 종료일이 프로젝트 시작일보다 빠르면 alert 를 발생하고,
-    // 프로젝트 종료일을 초기화한다.
-    $('#end-date').on("change", function () {
-        var startDate = $('#start-date').val();
-        var endDate = $('#end-date').val();
-
-        // 시작일과 종료일을 비교
-        if (startDate && endDate && endDate <= startDate) {
-            // 종료일이 시작일보다 이전이거나 같다면 경고를 표시하고 초기화
-            alert('종료일은 시작일보다 이후여야 합니다. 날짜를 다시 설정해주세요.');
-            $('#end-date').val(''); // 종료일 초기화
-        }
-    });
-
-    // 날짜 체크 2, 프로젝트 종료일이 선택된 후, 프로젝트 시작일을 수정하는 경우 alert 발생
-    // 프로젝트 시작일 유지
-    $('#start-date').on("change", function () {
-        var startDate = $('#start-date').val();
-        var endDate = $('#end-date').val();
-
-        if (startDate && endDate && endDate <= startDate) {
-            alert('시작일은 종료일보다 이전이여야 합니다. 날짜를 다시 설정해주세요.');
-            $('#start-date').val(''); // 시작일 초기화
-        }
-        }
-    );
-
-    // 날짜 최소, 최대 값 설정
-    var today = new Date();
-    var startOfYear = new Date(today.getFullYear(), 0, 1); // 올해의 1월 1일
-    var endOfFiveYears = new Date(today.getFullYear() + 5, 11, 31); // 5년 후의 12월 31일
-
-    // 날짜를 YYYY-MM-DD 형식으로 변환
-    var minStartDate = startOfYear.toISOString().slice(0, 10);
-    var maxEndDate = endOfFiveYears.toISOString().slice(0, 10);
-
-    // 시작일(input#start-date)에 min 속성 설정
-    $('#start-date').attr('min', minStartDate);
-    $('#start-date').attr('max', maxEndDate); // 시작일의 max를 종료일의 max와 동일하게 설정할 수 있습니다.
-
-    // 종료일(input#end-date)에 min과 max 속성 설정
-    $('#end-date').attr('min', minStartDate); // 종료일의 min을 시작일의 min과 동일하게 설정할 수 있습니다.
-    $('#end-date').attr('max', maxEndDate);
-
     // data-list Employee 기능 추가시 주석 해제
     // $('#pm-selector').on('input', function() {
     //     var value = $(this).val();
