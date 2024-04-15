8aeec98c243c496d0f68a7336cb6c3253c836157
Sohyun Kim <ksh96611@naver.com>
2024-04-15 17:12:45 +0900

    [2024-04-15][김소현]비품관리-기능구현1

--- src/main/java/com/ktdsuniversity/edu/pms/borrow/dao/BorrowDao.java
+++ src/main/java/com/ktdsuniversity/edu/pms/borrow/dao/BorrowDao.java
@@ -1,5 +1,25 @@
 package com.ktdsuniversity.edu.pms.borrow.dao;
 
+import java.util.List;
+
+import com.ktdsuniversity.edu.pms.borrow.vo.BorrowVO;
+import com.ktdsuniversity.edu.pms.employee.vo.EmployeeVO;
+
 public interface BorrowDao {
+	
+	public String NAME_SPACE = "com.ktdsuniversity.edu.pms.borrow.dao.BorrowDao";
+
+	/**
+	 * 사용자의 미반납 상태 비품 개수를 조회한다.
+	 * @param employeeVO
+	 * @return
+	 */
+	public int getBorrowCount(EmployeeVO employeeVO);
+
+	public List<BorrowVO> getUserRentalState(EmployeeVO employeeVO);
+
+	public int getProductManageStateAllCount();
+
+	public List<BorrowVO> getProductManageState();
 
 }
--- src/main/java/com/ktdsuniversity/edu/pms/product/service/ProductService.java
+++ src/main/java/com/ktdsuniversity/edu/pms/product/service/ProductService.java
@@ -1,5 +1,23 @@
 package com.ktdsuniversity.edu.pms.product.service;
 
+import com.ktdsuniversity.edu.pms.product.vo.ProductListVO;
+import com.ktdsuniversity.edu.pms.product.vo.ProductManagementVO;
+import com.ktdsuniversity.edu.pms.product.vo.ProductVO;
+
 public interface ProductService {
+
+	/**
+	 * 비픔 목록과 비품 수를 모두 조회
+	 * @return
+	 */
+	public ProductListVO getAllProduct();
+
+	public boolean createNewProduct(ProductVO productVO);
+
+	public ProductVO getOneProduct(String id);
+
+
+	
+	
 
 }
