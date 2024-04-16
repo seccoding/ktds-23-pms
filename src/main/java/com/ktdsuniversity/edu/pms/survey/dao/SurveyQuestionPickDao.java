400fd34c651580abf49d269639f776d2dbedda2f
Gyu Tae Sim <sims6574@gmail.com>
2024-04-16 18:26:22 +0900

    [2024-04-16][심규태] 충돌 수정

--- src/main/java/com/ktdsuniversity/edu/pms/survey/dao/SurveyQuestionPickDao.java
+++ src/main/java/com/ktdsuniversity/edu/pms/survey/dao/SurveyQuestionPickDao.java
@@ -1,7 +1,11 @@
 package com.ktdsuniversity.edu.pms.survey.dao;
 
+import com.ktdsuniversity.edu.pms.survey.vo.SurveyQuestionPickVO;
+
 public interface SurveyQuestionPickDao {
 	
 	public String NAME_SPACE = "com.ktdsuniversity.edu.pms.survey.dao.SurveyQuestionPickDao";
+
+	public int createNewAnswer(SurveyQuestionPickVO surveyQuestionPickVO);
 
 }
