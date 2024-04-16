b769a4fa40aab3245d67f4b730a2961d82628843
Min Chang Jang <mcjang1116@gmail.com>
2024-04-16 09:06:39 +0900

    [장민창] [2024-04-16] Error Page 커스터마이징 - error 폴더 아래에 statusCode 파일 추가하면 보여짐.

---
+++ src/main/java/com/ktdsuniversity/edu/pms/exceptions/web/CustomErrorController.java
@@ -0,0 +1,27 @@
+package com.ktdsuniversity.edu.pms.exceptions.web;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ktdsuniversity.edu.pms.utils.RequestUtil;
+
+import jakarta.servlet.RequestDispatcher;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+	@GetMapping("/error")
+	public String viewPageNotFoundErrorPage() {
+		Object status = RequestUtil.getRequest()
+				.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+		if (status != null) {
+			int statusCode = Integer.parseInt(status.toString());
+			return "error/" + statusCode;
+		}
+
+		return "error/404";
+	}
+
+}
