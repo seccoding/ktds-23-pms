ab1a446e80545e2bf265698f40b0420b5399cf5d
Soorim Hong <soosoorim@daum.net>
2024-04-19 17:54:30 +0900

    [2024-04-19] [홍수림] qna 기능 구현중

--- src/main/java/com/ktdsuniversity/edu/pms/qna/service/QnaServiceImpl.java
+++ src/main/java/com/ktdsuniversity/edu/pms/qna/service/QnaServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ktdsuniversity.edu.pms.qna.service;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +9,11 @@ import org.springframework.stereotype.Service;
 
 import com.ktdsuniversity.edu.pms.beans.FileHandler;
 import com.ktdsuniversity.edu.pms.knowledge.service.KnowledgeServiceImpl;
+import com.ktdsuniversity.edu.pms.knowledge.vo.KnowledgeListVO;
 import com.ktdsuniversity.edu.pms.qna.dao.QnaDao;
+import com.ktdsuniversity.edu.pms.qna.vo.QnaListVO;
+import com.ktdsuniversity.edu.pms.qna.vo.QnaVO;
+import com.ktdsuniversity.edu.pms.qna.vo.SearchQnaVO;
 
 @Service
 public class QnaServiceImpl implements QnaService{
@@ -19,5 +25,53 @@ public class QnaServiceImpl implements QnaService{
 	
 	@Autowired
 	private FileHandler fileHandler;
+
+
+	@Override
+	public QnaListVO getAllQna() {
+		
+		int qnaCount = this.qnaDao.getAllQnaCount();
+		List<QnaVO> qnaList = this.qnaDao.getAllQna();
+		
+		QnaListVO qnaListVO = new QnaListVO();
+		qnaListVO.setQnaCnt(qnaCount);
+		qnaListVO.setQnaList(qnaList);
+		
+		return qnaListVO;
+	}
+	
+	@Override
+	public QnaListVO searchAllQna(SearchQnaVO searchQnaVO) {
+
+		return null;
+	}
+
+	@Override
+	public QnaVO getOneQna(String qaId, boolean isIncrease) {
+
+		QnaVO qnaVO = this.qnaDao.selectOneQna(qaId);
+		
+		// 게시글 조회한게 결과가 없다면.예외 발생시키기
+		if (qnaVO == null) {
+//			throw new PageNotFoundException(); TO DO!!
+		}
+		
+		// 조회수 1증가
+		if(isIncrease) {
+			int updatedCount = this.qnaDao.increaseViewCount(qaId);
+		}
+		
+		return qnaVO;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
