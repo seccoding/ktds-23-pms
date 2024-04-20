2b0e197521b63886f872c4bcd29eac2100b3d66e
CHOI, HYUNGJUN <2018s4030@gs.anyang.ac.kr>
2024-04-20 18:17:40 +0900

    [2024-04-20] [최형준] 메뉴 API 구현 - 수정 필요

---
+++ src/main/java/com/ktdsuniversity/edu/pms/menu/dao/MenuDao.java
@@ -0,0 +1,13 @@
+package com.ktdsuniversity.edu.pms.menu.dao;
+
+import com.ktdsuniversity.edu.pms.menu.vo.MenuVO;
+
+import java.util.List;
+
+public interface MenuDao {
+
+    public static final String NAME_SPACE = "com.ktdsuniversity.edu.pms.menu.dao.MenuDao";
+
+    List<MenuVO> selectAllMenuList();
+
+}
