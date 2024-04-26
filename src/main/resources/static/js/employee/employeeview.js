834fe7e35451299345f6196aee814f241592f9b9
Sujin Park <sujin978@naver.com>
2024-04-26 17:55:50 +0900

    [2024-04-26][박수진] employeeview.js  중복수정

--- src/main/resources/static/js/employee/employeeview.js
+++ src/main/resources/static/js/employee/employeeview.js
@@ -16,7 +16,8 @@ $().ready(function (){
         }
     })
 
-    $(".change-pstn").on("click", function(){
+   
+        $(".change-pstn").on("click", function(){
         $(".pstn-select-box").val($(".pstn-select-box").data("origin"))
         $(".pstn-modal")[0].showModal()
     })
@@ -25,26 +26,60 @@ $().ready(function (){
         $(".pstn-modal")[0].close()
     })
 
-    $(".change-pstn-btn").on("click", function(){
-        var selectPstnId = $(".pstn-select-box").val()
-        var pastPstnId = $(".pstn-select-box").data("origin")
-        console.log(pastPstnId)
-        console.log(selectPstnId)
-        if(selectPstnId==pastPstnId){
+    $(".change-position-btn").on("click", function(){
+        var selectPositionId = $(".position-select-box").val()
+        var pastPositionId = $(".position-select-box").data("origin")
+        console.log(pastPositionId)
+        console.log(selectPositionId)
+        if(selectPositionId==pastPositionId){
             alert("동일한 직급으로 변경할 수 없습니다.")
             return
         }
-        var reason = $("#pstn-change-note").val()
+        var reason = $("#position-change-note").val()
         $.post("/ajax/change/position", {
             empId:empId,
-            pstnId:selectPstnId,
-            "positionHistoryVO.pastPstnId":pastPstnId,
+            positionId:selectPositionId,
+            "positionHistoryVO.pastPositionId":pastPositionId,
             "positionHistoryVO.cnNote":reason,
         }, function(res){
             if(res.data.isSuccess){
                 alert("직급을 변경했습니다.")
             }else{
                 alert("직급 변경 중 오류가 발생했습니다.")
+            }
+            location.href = res.data.next
+        })
+    })
+
+    $(".change-job").on("click", function(){
+        $(".job-select-box").val($(".job-select-box").data("origin"))
+        $(".job-modal")[0].showModal()
+    })
+
+    $(".change-job-cancel").on("click", function(){
+        $(".job-modal")[0].close()
+    })
+
+    $(".change-job-btn").on("click", function(){
+        var selectjobId = $(".job-select-box").val()
+        var pastjobId = $(".job-select-box").data("origin")
+        console.log(pastjobId)
+        console.log(selectjobId)
+        if(selectJobId==pastJobId){
+            alert("동일한 직무로 변경할 수 없습니다.")
+            return
+        }
+        var reason = $("#job-change-note").val()
+        $.post("/ajax/change/jobHistory", {
+            empId:empId,
+            jobId:selectjobId,
+            "jobHistoryVO.pastjobId":pastjobId,
+            "jobHistoryVO.cnNote":reason,
+        }, function(res){
+            if(res.data.isSuccess){
+                alert("직무를 변경했습니다.")
+            }else{
+                alert("직무 변경 중 오류가 발생했습니다.")
             }
             location.href = res.data.next
         })
