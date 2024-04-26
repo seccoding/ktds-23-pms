b427751f335b21d568cd9de6e8131e79c8046c19
Jae Hyoun Park <jae220011@gmail.com>
2024-04-26 17:48:03 +0900

    [2024-04-26][박재현]로그인 기능 구현 - 회원가입 select 변경

--- src/main/java/com/ktdsuniversity/edu/pms/employee/web/EmployeeController.java
+++ src/main/java/com/ktdsuniversity/edu/pms/employee/web/EmployeeController.java
@@ -1,8 +1,13 @@
 package com.ktdsuniversity.edu.pms.employee.web;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.poi.ss.usermodel.DataFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -224,24 +229,22 @@ public class EmployeeController {
 			return new AjaxResponse().append("errorMessage", "이미 사용중인 사원번호 입니다.");
 		}
 		
+		
 		Validator<EmployeeVO> validator = new Validator<>(employeeVO);
 		
+		LocalDate.now().toString();
 		validator.add("empId", Type.NOT_EMPTY, "사원번호를 입력해 주세요.")
-				.add("empId", Type.EMPID, "사원번호 형식으로 입력해 주세요")
-				.add("pwd", Type.NOT_EMPTY, "비밀번호를 입력해 주세요")
-				.add("pwd", Type.PASSWORD, "비밀번호 형식으로 입력해 주세요")
-				.add("empName", Type.NOT_EMPTY, "사원이름을 입력해 주세요")
-				.add("hireDt", Type.NOT_EMPTY, "입사일을 지정해 주세요")
-				.add("addr", Type.NOT_EMPTY, "주소를 입력해 주세요")
-				.add("brth", Type.NOT_EMPTY, "생일을 지정해 주세요")
-				.add("email", Type.NOT_EMPTY, "이메일을 입력해 주세요")
-				.add("email", Type.EMAIL, "이메일 형식으로 입력해 주세요")
-				.add("deptId", Type.NOT_EMPTY, "부서ID를 입력해 주세요")
-				.add("deptId", Type.DEPTID, "부서ID형식으로 입력해 주세요")
-				.add("jobId", Type.NOT_EMPTY, "직무ID를 입력해 주세요")
-				.add("jobId", Type.JOBID, "직무ID형식으로 입력해주세요")
-				.add("pstnId", Type.NOT_EMPTY, "직급ID를 입력해 주세요")
-				.add("pstnId", Type.PSTNID, "직급ID형식으로 입력해 주세요").start();
+				.add("empId", Type.EMPID, "사원번호 형식으로 입력해 주세요.")
+				.add("pwd", Type.NOT_EMPTY, "비밀번호를 입력해 주세요.")
+				.add("pwd", Type.PASSWORD, "비밀번호 형식으로 입력해 주세요.")
+				.add("empName", Type.NOT_EMPTY, "사원이름을 입력해 주세요.")
+				.add("hireDt", Type.NOT_EMPTY, "입사일을 지정해 주세요.")
+//				.add("hireDt", Type.DATE, String.format("yyyy-MM-dd"), "입사일은 현재 날짜보다 이전이어야 합니다.")
+				.add("addr", Type.NOT_EMPTY, "주소를 입력해 주세요.")
+				.add("brth", Type.NOT_EMPTY, "생일을 지정해 주세요.")
+				.add("email", Type.NOT_EMPTY, "이메일을 입력해 주세요.")
+				.add("email", Type.EMAIL, "이메일 형식으로 입력해 주세요.").start();
+		
 		
 		if (validator.hasErrors()) {
 			Map<String, List<String>> errors = validator.getErrors();
