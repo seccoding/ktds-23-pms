7cc89aa6a2cfd6df3f58e283d6b9f22c33bce48b
211-20\User <kwan0507@nate.com>
2024-04-30 11:24:38 +0900

    Revert "[2024-04-30] [김형관] yml 파일 업로드, 이슈  일괄삭제 에러 수정"
    This reverts commit f4d5a98e0bf28d2c03a45b5e4abdb49e68a7519f.

--- src/main/resources/static/js/issue/issuelist.js
+++ src/main/resources/static/js/issue/issuelist.js
@@ -7,34 +7,33 @@ $().ready(function () {
     });
 
     $("#deleteMassiveIssue").on("click", function() {
+      loadModal({
+        content: "삭제할 이슈를 선택하세요.",
+        fnPositiveBtnHandler: function() {
+          location.reload();
+        },
+        showNegativeBtn: false
+      });
       var checkedItems = $(".target-issue-id:checked");
       var itemsArray = [];
-      checkedItems.each(function (index, data) {
+        checkedItems.each(function (index, data) {
         itemsArray.push($(data).val());
       });
-
-      if (itemsArray.length === 0) {
-        loadModal({
-          content: "삭제할 이슈를 선택하세요.",
-          showNegativeBtn: false
-        });
-      } else {
-        loadModal({
-          content: "이슈를 일괄 삭제 하시겠습니까?",
-          fnPositiveBtnHandler: function() {
-            $.post(
-              "/ajax/issue/delete/massive",
-              { deleteItems: itemsArray },
-              function(response) {
-                var result = response.data.result;
-                if (result) {
-                  location.reload();
-                }
+      $.post(
+        "/ajax/issue/delete/massive",
+        { deleteItems: itemsArray },
+        function(response) {
+          var result = response.data.result;
+          if (result) {
+            loadModal({
+              content: "이슈를 일괄삭제 하시겠습니까?",
+              fnPositiveBtnHandler: function() {
+                location.reload();
               }
-            )
+            })
           }
-        });
-      }
+        }
+      )
     });
 
     $("#checked-all").on("change", function () {
