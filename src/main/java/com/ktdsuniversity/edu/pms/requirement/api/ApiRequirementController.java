e77acfebdd289aa570ff18f6f0b3880d74c469e8
seunguuu <youu0208@naver.com>
2024-06-03 14:42:07 +0900

    [2024-06-03][유승훈] 요구사항 Controller 메소드 추가

--- src/main/java/com/ktdsuniversity/edu/pms/requirement/api/ApiRequirementController.java
+++ src/main/java/com/ktdsuniversity/edu/pms/requirement/api/ApiRequirementController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.ktdsuniversity.edu.pms.beans.security.SecurityUser;
 import com.ktdsuniversity.edu.pms.commoncode.service.CommonCodeService;
@@ -16,9 +18,11 @@ import com.ktdsuniversity.edu.pms.commoncode.vo.CommonCodeVO;
 import com.ktdsuniversity.edu.pms.employee.vo.EmployeeVO;
 import com.ktdsuniversity.edu.pms.project.service.ProjectService;
 import com.ktdsuniversity.edu.pms.project.vo.ProjectListVO;
+import com.ktdsuniversity.edu.pms.project.vo.ProjectTeammateVO;
 import com.ktdsuniversity.edu.pms.requirement.service.RequirementService;
 import com.ktdsuniversity.edu.pms.requirement.vo.RequirementListVO;
 import com.ktdsuniversity.edu.pms.requirement.vo.RequirementSearchVO;
+import com.ktdsuniversity.edu.pms.requirement.vo.RequirementVO;
 import com.ktdsuniversity.edu.pms.utils.ApiResponse;
 
 @RestController
@@ -63,5 +67,35 @@ public class ApiRequirementController {
 				requirementList.getCount(),
 				requirementSearchVO.getPageCount(), 
 				requirementSearchVO.getPageNo() < requirementSearchVO.getPageCount() - 1);
+	}
+	
+	
+	@GetMapping("/requirement/view")
+	public ApiResponse getOneRequirement(Authentication authentication, 
+			@RequestParam("prjId") String prjId, @RequestParam("rqmId") String rqmId) {
+		
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+		EmployeeVO employeeVO = ((SecurityUser) userDetails).getEmployeeVO();
+		
+		// 본인 프로젝트가 아닐경우, 잘못된 프로젝트 아이디가 입력된경우 에러페이지 & 메시지 전달
+		if (employeeVO.getAdmnCode() != "301") {
+			List<ProjectTeammateVO> tmList = this.projectService.getAllProjectTeammateByProjectId(prjId).stream()
+					.filter(tm -> tm.getTmId().equals(employeeVO.getEmpId())).toList();
+			
+		}
+
+		boolean isPmAndPl = false;
+		List<ProjectTeammateVO> tmList = this.projectService.getAllProjectTeammateByProjectId(prjId).stream()
+				.filter(tm -> tm.getTmId().equals(employeeVO.getEmpId()))
+				.filter(tm -> tm.getRole().equals("PM") || tm.getRole().equals("PL")).toList();
+		if (tmList.size() > 0) {
+			isPmAndPl = true;
+		}
+
+		RequirementVO requirement = this.requirementService.getOneRequirement(rqmId);
+		
+
+		return ApiResponse.Ok(requirement, requirement == null ? 0 : 1);
+
 	}
 }
