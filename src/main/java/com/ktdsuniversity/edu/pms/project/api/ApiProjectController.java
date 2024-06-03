1a3564b9825e5314bd92b93b3775a3e666a5ce03
Sohyun Kim <ksh96611@naver.com>
2024-06-03 14:32:11 +0900

    [2024-06-03][김소현]api 수정중

--- src/main/java/com/ktdsuniversity/edu/pms/project/api/ApiProjectController.java
+++ src/main/java/com/ktdsuniversity/edu/pms/project/api/ApiProjectController.java
@@ -2,32 +2,38 @@ package com.ktdsuniversity.edu.pms.project.api;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.ktdsuniversity.edu.pms.beans.security.SecurityUser;
 import com.ktdsuniversity.edu.pms.commoncode.service.CommonCodeService;
 import com.ktdsuniversity.edu.pms.commoncode.vo.CommonCodeVO;
 import com.ktdsuniversity.edu.pms.department.service.DepartmentService;
+import com.ktdsuniversity.edu.pms.department.vo.DepartmentVO;
 import com.ktdsuniversity.edu.pms.employee.service.EmployeeService;
 import com.ktdsuniversity.edu.pms.employee.vo.EmployeeVO;
+import com.ktdsuniversity.edu.pms.exceptions.CreationException;
 import com.ktdsuniversity.edu.pms.project.service.ProjectService;
+import com.ktdsuniversity.edu.pms.project.vo.CreateProjectVO;
 import com.ktdsuniversity.edu.pms.project.vo.ProjectListVO;
 import com.ktdsuniversity.edu.pms.project.vo.ProjectTeammateVO;
 import com.ktdsuniversity.edu.pms.project.vo.ProjectVO;
 import com.ktdsuniversity.edu.pms.project.vo.SearchProjectVO;
 import com.ktdsuniversity.edu.pms.requirement.service.RequirementService;
 import com.ktdsuniversity.edu.pms.requirement.vo.RequirementVO;
+import com.ktdsuniversity.edu.pms.utils.AjaxResponse;
 import com.ktdsuniversity.edu.pms.utils.ApiResponse;
+import com.ktdsuniversity.edu.pms.utils.Validator;
+import com.ktdsuniversity.edu.pms.utils.Validator.Type;
 
 @RestController
 @RequestMapping("/api/project")
@@ -50,7 +56,7 @@ public class ApiProjectController {
 	    
 	    // getAllProject + getAllProjectByProjectTeammateRole
 	    @GetMapping("/search")
-	    public ApiResponse viewSearchProjectListPage(SearchProjectVO searchProjectVO,Authentication authentication) {
+	    public ApiResponse getSearchProjectListPage(SearchProjectVO searchProjectVO,Authentication authentication) {
 
 	    	UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 			EmployeeVO employeeVO = ((SecurityUser) userDetails).getEmployeeVO();
@@ -61,18 +67,18 @@ public class ApiProjectController {
 	        List<CommonCodeVO> projectCommonCodeList = commonCodeService.getAllCommonCodeListByPId("400");
 			
 	        List<Object> dataList = new ArrayList<>();
-			dataList.add(projectListVO);
 			dataList.add(projectCommonCodeList);
+			dataList.add(projectListVO);
 
 	        return ApiResponse.Ok(dataList);
 	    }
 
 	    
 	    @GetMapping("/view/{prjId}")
-	    public ApiResponse viewProjectDetailPage(@PathVariable String prjId, Authentication authentication) {
+	    public ApiResponse getProjectDetailPage(@PathVariable String prjId, Authentication authentication) {
 	    	UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 			EmployeeVO employeeVO = ((SecurityUser) userDetails).getEmployeeVO();
-			List<String> errorMessages = null;
+			List<String> errorMessages = new ArrayList<>();
 
 	        ProjectVO projectVO = projectService.getOneProject(prjId);
 	        int projectTeammateCount = projectService.getProjectTeammateCount(prjId);
@@ -103,6 +109,64 @@ public class ApiProjectController {
 	        }
 
 	        
+	    }
+	    
+	    @GetMapping("/write")
+	    public ApiResponse getProjectWritePage(Authentication authentication) {
+	    	UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+			EmployeeVO employeeVO = ((SecurityUser) userDetails).getEmployeeVO();
+	        // 검증로직, 프로젝트 생성은 관리자만 가능하다.
+	        if (!employeeVO.getAdmnCode().equals("301")) {
+	        	return ApiResponse.FORBIDDEN("접근 권한이 없습니다.");
+	        }
+
+	        List<Object> dataList = new ArrayList<>();
+	        List<EmployeeVO> employeeList = employeeService.getAllEmployee().getEmployeeList();
+	        List<DepartmentVO> departmentList = departmentService.getOnlyDepartment().getDepartmentList();
+
+	        dataList.add(employeeList);
+	        dataList.add(departmentList);
+	        return ApiResponse.Ok(dataList);
+	    }
+
+	    @PostMapping("/write")
+	    public ApiResponse doWriteProject(CreateProjectVO createProjectVO, Authentication authentication) {
+	    	UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+			EmployeeVO employeeVO = ((SecurityUser) userDetails).getEmployeeVO();
+			
+	        // 검증로직, 프로젝트 생성은 관리자만 가능하다.
+	        if (!employeeVO.getAdmnCode().equals("301")) {
+	        	return ApiResponse.FORBIDDEN("접근 권한이 없습니다.");
+	        }
+
+	        Validator<CreateProjectVO> validator = new Validator<>(createProjectVO);
+
+	        validator.add("prjName", Type.NOT_EMPTY, "프로젝트명을 입력해주세요.")
+	                .add("clntInfo", Type.NOT_EMPTY, "고객사를 입력해주세요.")
+	                .add("deptId", Type.NOT_EMPTY, "담당부서를 선택해주세요.")
+	                .add("strtDt", Type.NOT_EMPTY, "시작일을 입력해주세요.")
+	                .add("pmId", Type.NOT_EMPTY, "담당자를 입력해주세요.")
+	                .add("endDt", Type.NOT_EMPTY, "종료일을 입력해주세요.")
+	                .add("strtDt", Type.DATE, createProjectVO.getEndDt(), "종료일은 시작일보다 이후여야 합니다. 날짜를 다시 설정해주세요")
+	                .start();
+
+	        if (validator.hasErrors()) {
+	            Map<String, List<String>> errors = validator.getErrors();
+//	            return ApiResponse.BAD_REQUEST(errorList);
+	            return null;
+	        }
+
+	        // 2. 검증 로직에 잘 맞춰서 작성한 경우, 데이터 저장
+	        // -> 세션에서 작성자 id 추출
+	        createProjectVO.setCrtrId(employeeVO.getEmpId());
+
+	        boolean isCreateSuccess = projectService.createNewProject(createProjectVO);
+
+	        if (!isCreateSuccess) {
+	            return ApiResponse.FORBIDDEN("프로젝트 생성 중 문제가 발생하였습니다.");
+	        }
+
+	        return ApiResponse.Ok(isCreateSuccess);
 	    }
 
 }
---
+++ src/main/java/com/ktdsuniversity/edu/pms/project/dao/ClientDao.java
@@ -0,0 +1,7 @@
+package com.ktdsuniversity.edu.pms.project.dao;
+
+public interface ClientDao {
+	
+	public String NAME_SPACE = "com.ktdsuniversity.edu.pms.project.dao.ClientDao";
+
+}
