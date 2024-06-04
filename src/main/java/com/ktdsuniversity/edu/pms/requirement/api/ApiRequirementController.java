89129437512d09371a75b318b44967913c287f98
seunguuu <youu0208@naver.com>
2024-06-04 15:59:50 +0900

    [2024-06-04][유승훈] 요구사항 작성 페이지 추가

--- src/main/java/com/ktdsuniversity/edu/pms/requirement/api/ApiRequirementController.java
+++ src/main/java/com/ktdsuniversity/edu/pms/requirement/api/ApiRequirementController.java
@@ -1,12 +1,15 @@
 package com.ktdsuniversity.edu.pms.requirement.api;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +26,7 @@ import com.ktdsuniversity.edu.pms.requirement.service.RequirementService;
 import com.ktdsuniversity.edu.pms.requirement.vo.RequirementListVO;
 import com.ktdsuniversity.edu.pms.requirement.vo.RequirementSearchVO;
 import com.ktdsuniversity.edu.pms.requirement.vo.RequirementVO;
+import com.ktdsuniversity.edu.pms.utils.AjaxResponse;
 import com.ktdsuniversity.edu.pms.utils.ApiResponse;
 
 @RestController
@@ -97,5 +101,48 @@ public class ApiRequirementController {
 
 		return ApiResponse.Ok(requirement, requirement == null ? 0 : 1);
 
+	}
+	
+	
+	@GetMapping("/requirement/write")
+	public ApiResponse getRequirementWritePage(Authentication authentication) {
+		
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+		EmployeeVO employeeVO = ((SecurityUser) userDetails).getEmployeeVO();
+		
+		ProjectListVO projectList = this.projectService.getAllProject();
+		if (!employeeVO.getAdmnCode().equals("301")) {// 관리자가 아니면
+			projectList.setProjectList(this.projectService.getAllProjectByProjectTeammateId(employeeVO.getEmpId()));
+		} else {// 관리자라면
+
+		}
+		projectList.setProjectList(
+				projectList.getProjectList().stream().toList());
+
+		List<CommonCodeVO> scdStsList = this.commonCodeService.getAllCommonCodeListByPId("500");
+		List<CommonCodeVO> rqmStsList = this.commonCodeService.getAllCommonCodeListByPId("600");
+		
+		
+		// 모든 데이터를 하나의 응답으로 합치기
+		Map <String, Object> responseData = new HashMap<>();
+		responseData.put("projectList", projectList.getProjectList());
+		responseData.put("scdSts", scdStsList);
+		responseData.put("rqmSts", rqmStsList);
+		
+//		System.out.println("@@@@@@@@@@@@@@" + responseData.get("projectList"));
+//		System.out.println("##############" + responseData.get("scdSts"));
+//		System.out.println("$$$$$$$$$$$$$$" + responseData.get("rqmSts"));
+
+		return ApiResponse.Ok(responseData, responseData == null ? 0 : 1);
+
+	}
+	
+	
+	@GetMapping("/requirement/write/{prjId}")
+	public ApiResponse getProjectTeammate(@PathVariable String prjId, Authentication authentication) {
+		List<ProjectTeammateVO> prjTeammateList = this.projectService.getAllProjectTeammateByProjectId(prjId);
+		System.out.println("@@@@@@@@@@@prjTeammateList: " + prjTeammateList);
+//		return new AjaxResponse().append("prjTeammateList", prjTeammateList);
+		return ApiResponse.Ok(prjTeammateList);
 	}
 }
