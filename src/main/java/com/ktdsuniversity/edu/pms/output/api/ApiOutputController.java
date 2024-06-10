8cb856268ffa78a0756c8588bf467d76b9d5e805
seunguuu <youu0208@naver.com>
2024-06-10 17:59:31 +0900

    [2024-06-10][유승훈] 요구사항, 산출물 CRUD 기능추가

--- src/main/java/com/ktdsuniversity/edu/pms/output/api/ApiOutputController.java
+++ src/main/java/com/ktdsuniversity/edu/pms/output/api/ApiOutputController.java
@@ -1,15 +1,23 @@
 package com.ktdsuniversity.edu.pms.output.api;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ktdsuniversity.edu.pms.beans.security.SecurityUser;
@@ -26,6 +34,7 @@ import com.ktdsuniversity.edu.pms.project.dao.ProjectDao;
 import com.ktdsuniversity.edu.pms.project.service.ProjectService;
 import com.ktdsuniversity.edu.pms.project.vo.ProjectTeammateVO;
 import com.ktdsuniversity.edu.pms.project.vo.ProjectVO;
+import com.ktdsuniversity.edu.pms.utils.AjaxResponse;
 import com.ktdsuniversity.edu.pms.utils.ApiResponse;
 import com.ktdsuniversity.edu.pms.utils.Validator;
 import com.ktdsuniversity.edu.pms.utils.Validator.Type;
@@ -47,8 +56,6 @@ public class ApiOutputController {
 	@GetMapping("/output/search")
 	public ApiResponse getOutputList( 
 			OutputSearchVO outputSearchVO, Authentication authentication) {
-		
-		System.out.println("######" + authentication + "#########");
 		
 		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 		EmployeeVO employeeVO = ((SecurityUser) userDetails).getEmployeeVO();
@@ -75,6 +82,153 @@ public class ApiOutputController {
 				outputList.getListCnt(), 
 				outputSearchVO.getPageCount(), 
 				outputSearchVO.getPageNo() < outputSearchVO.getPageCount() - 1);
+	}
+	
+	
+	@GetMapping("/output/write")
+	public ApiResponse getOutputWritePage(Authentication authentication) {
+		
+		// 산출물 작성과 수정에 필요한 데이터들을 불러오는 API
+		
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+		EmployeeVO employeeVO = ((SecurityUser) userDetails).getEmployeeVO();
+		
+		this.checkAccess(employeeVO);
+		
+
+		List<ProjectVO> projectList;
+		if(employeeVO.getAdmnCode().equals("301")) {
+			projectList=this.projectService.getAllProject().getProjectList();
+		}else {
+			 projectList = this.projectService.getAllProjectByProjectTeammateId(employeeVO.getEmpId());
+
+		}
+		projectList =projectList.stream().toList();
+		List<CommonCodeVO> outputType = this.commonCodeService.getAllCommonCodeListByPId("1000");
+		List<CommonCodeVO> prjSts = this.commonCodeService.getAllCommonCodeListByPId("400");
+		
+		// 모든 데이터를 하나의 응답으로 합치기
+		Map <String, Object> responseData = new HashMap<>();
+		responseData.put("projectList", projectList);
+		responseData.put("outputType", outputType);
+		responseData.put("prjSts", prjSts);
+		
+		return ApiResponse.Ok(responseData, responseData == null ? 0 : 1);
+	}
+	
+	
+	@PostMapping("/output/write")
+	public ApiResponse createOutput(Authentication authentication, 
+			@RequestParam MultipartFile file, OutputVO outputVO) {
+		
+		// 입력한 값들을 전송하는 API
+		
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+		EmployeeVO employeeVO = ((SecurityUser) userDetails).getEmployeeVO();
+		
+		this.checkAccess(employeeVO, outputVO.getPrjId());
+		
+		Map<String,List<String>> error = this.outputvalidator(outputVO, file);
+		if(error != null) {
+			return ApiResponse.Ok(error);
+		}
+		
+		outputVO.setCrtrId(employeeVO.getEmpId());
+		boolean isSuccess = this.outputService.insertOneOutput(outputVO, file);
+		
+		return ApiResponse.Ok(isSuccess);
+	}
+	
+	
+	@GetMapping("/output/modify/{outId}")
+	public ApiResponse getOutputModifyPage(Authentication authentication, @PathVariable String outId) {
+		
+		// 수정 페이지에서 필요한 값들을 가져오는 API
+		
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+		EmployeeVO employeeVO = ((SecurityUser) userDetails).getEmployeeVO();
+		
+		this.checkAccess(employeeVO);
+
+		List<ProjectVO> projectList;
+		if(employeeVO.getAdmnCode().equals("301")) {
+			projectList = this.projectService.getAllProject().getProjectList();
+		}else {
+			 projectList = this.projectService.getAllProjectByProjectTeammateId(employeeVO.getEmpId());
+
+		}
+		projectList = projectList.stream().toList();
+		List<CommonCodeVO> outputType = this.commonCodeService.getAllCommonCodeListByPId("1000");
+		List<CommonCodeVO> prjSts = this.commonCodeService.getAllCommonCodeListByPId("400");
+		
+		OutputVO output = this.outputService.getOneOutput(outId);
+		
+		if(this.throwUnauthorizedUser(employeeVO,output.getCrtrId())) {
+			throw new AccessDeniedException();		
+			}
+		
+		// 모든 데이터를 하나의 응답으로 합치기
+		Map <String, Object> responseData = new HashMap<>();
+		responseData.put("output", output);
+		responseData.put("projectList", projectList);
+		responseData.put("outputType", outputType);
+		responseData.put("prjSts", prjSts);
+
+		return ApiResponse.Ok(responseData, responseData == null ? 0 : 1);
+
+	}
+
+	@PutMapping("/output/modify/{outId}")
+	public ApiResponse modifyOutput(Authentication authentication, @PathVariable String outId,
+			@RequestParam MultipartFile file, OutputVO outputVO) {
+		
+		// 수정한 값들을 전송하는 API
+		
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+		EmployeeVO employeeVO = ((SecurityUser) userDetails).getEmployeeVO();
+		
+		this.checkAccess(employeeVO, outputVO.getPrjId());
+
+		Map<String,List<String>> error = this.outputvalidator(outputVO, file);
+		if(error != null) {
+			return ApiResponse.Ok(error);
+		}
+
+		boolean isSuccess = this.outputService.updateOneOutput(outputVO, file);
+		
+		return ApiResponse.Ok(isSuccess);
+	}
+	
+	
+	@DeleteMapping("/output/delete/{outId}")
+	public ApiResponse deleteOutputment(Authentication authentication, @PathVariable String outId) {
+		
+		// 산출물 게시물을 삭제하는 API
+		
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+		EmployeeVO employeeVO = ((SecurityUser) userDetails).getEmployeeVO();
+		
+		this.checkAccess(employeeVO);
+		OutputVO output = this.outputService.getOneOutput(outId);
+		if(this.throwUnauthorizedUser(employeeVO,output.getCrtrId())) {
+			return ApiResponse.Ok(false);
+			
+			}
+		boolean isSuccess = this.outputService.deleteOneOutput(outId);	
+		
+		return ApiResponse.Ok(isSuccess);
+	}
+	
+	
+	@GetMapping("output/downloadFile/{outId}")
+	public ResponseEntity<Resource> fileDownload(@SessionAttribute("_LOGIN_USER_") EmployeeVO employeeVO,
+			@PathVariable String outId) {
+		this.checkAccess(employeeVO);
+
+		OutputVO Output = this.outputService.getOneOutput(outId);
+
+		return this.outputService.getDownloadFile(Output);
+
 	}
 	
 	
@@ -106,7 +260,7 @@ public class ApiOutputController {
 		}
 	}
 	
-	private Map<String , List<String>> outputvalidator (OutputVO  outputVO , MultipartFile file){
+	private Map<String , List<String>> outputvalidator (OutputVO outputVO, MultipartFile file){
 		if(file !=null && ! file.isEmpty()) {
 			outputVO.setOutFile(file.getOriginalFilename());
 		}
@@ -114,9 +268,9 @@ public class ApiOutputController {
 		Validator<OutputVO> validator = new Validator<>(outputVO);
 		validator
 		.add("outTtl", Type.NOT_EMPTY, "제목은 필수 입력값입니다")
-		.add("outType", Type.NOT_EMPTY, "산출물 타입은 필수 입력값입니다")
-		.add("prjId", Type.NOT_EMPTY, "올바르지 않은 프로젝트에서 생성했습니다.")
-		.add("outFile", Type.NOT_EMPTY, "파일은 필수입력값입니다")
+		.add("prjId", Type.NOT_EMPTY, "프로젝트는 필수 입력값입니다")
+		.add("outType", Type.NOT_EMPTY, "산출물 종류은 필수 입력값입니다")
+		.add("outVer", Type.NOT_EMPTY, "프로젝트 진행상황은 필수 입력값입니다")
 		.start();
 	
 		
